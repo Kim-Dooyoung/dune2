fd4ba4869f2ff67a1e10a1f9a1129e7ad531a8df
Kim_Dooyoung <dstellao016@gmail.com>
2024-12-10 00:39:54 +0900

    1) 준비

---
+++ Project1/display.c
@@ -0,0 +1,105 @@
+/*
+*  display.c:
+* 화면에 게임 정보를 출력
+* 맵, 커서, 시스템 메시지, 정보창, 자원 상태 등등
+* io.c에 있는 함수들을 사용함
+*/
+
+#include "display.h"
+#include "io.h"
+
+// 출력할 내용들의 좌상단(topleft) 좌표
+const POSITION resource_pos = { 0, 0 };
+const POSITION map_pos = { 1, 0 };
+
+
+char backbuf[MAP_HEIGHT][MAP_WIDTH] = { 0 };
+char frontbuf[MAP_HEIGHT][MAP_WIDTH] = { 0 };
+
+void project(char src[N_LAYER][MAP_HEIGHT][MAP_WIDTH], char dest[MAP_HEIGHT][MAP_WIDTH]);
+void display_resource(RESOURCE resource);
+void display_map(char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH]);
+void display_cursor(CURSOR cursor);
+void display_system_message(const char* message);
+void display_commands(const char* commands);
+void display_status(const char* status);
+
+void display(
+	RESOURCE resource,
+	char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH],
+	CURSOR cursor)
+{
+	display_resource(resource);
+	display_map(map);
+	display_cursor(cursor);
+	display_system_message("All systems operational.");
+	display_commands("Move, Attack, Defend");
+	display_status("Unit: Worker, HP: 100/100");
+	// ...
+}
+
+void display_resource(RESOURCE resource) {
+	set_color(COLOR_RESOURCE);
+	gotoxy(resource_pos);
+	printf("spice = %d/%d, population=%d/%d\n",
+		resource.spice, resource.spice_max,
+		resource.population, resource.population_max
+	);
+}
+
+// subfunction of draw_map()
+void project(char src[N_LAYER][MAP_HEIGHT][MAP_WIDTH], char dest[MAP_HEIGHT][MAP_WIDTH]) {
+	for (int i = 0; i < MAP_HEIGHT; i++) {
+		for (int j = 0; j < MAP_WIDTH; j++) {
+			for (int k = 0; k < N_LAYER; k++) {
+				if (src[k][i][j] >= 0) {
+					dest[i][j] = src[k][i][j];
+				}
+			}
+		}
+	}
+}
+
+void display_map(char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH]) {
+	project(map, backbuf);
+
+	for (int i = 0; i < MAP_HEIGHT; i++) {
+		for (int j = 0; j < MAP_WIDTH; j++) {
+			if (frontbuf[i][j] != backbuf[i][j]) {
+				POSITION pos = { i, j };
+				printc(padd(map_pos, pos), backbuf[i][j], COLOR_DEFAULT);
+			}
+			frontbuf[i][j] = backbuf[i][j];
+		}
+	}
+}
+
+// frontbuf[][]에서 커서 위치의 문자를 색만 바꿔서 그대로 다시 출력
+void display_cursor(CURSOR cursor) {
+	POSITION prev = cursor.previous;
+	POSITION curr = cursor.current;
+
+	char ch = frontbuf[prev.row][prev.column];
+	printc(padd(map_pos, prev), ch, COLOR_DEFAULT);
+
+	ch = frontbuf[curr.row][curr.column];
+	printc(padd(map_pos, curr), ch, COLOR_CURSOR);
+}
+
+void display_system_message(const char* message) {
+	POSITION message_pos = { MAP_HEIGHT + 1, 0 };
+	gotoxy(message_pos);
+	printf("SYSTEM: %s", message);
+}
+
+void display_commands(const char* commands) {
+	POSITION command_pos = { MAP_HEIGHT + 2, 0 };
+	gotoxy(command_pos);
+	printf("COMMANDS: %s", commands);
+}
+
+void display_status(const char* status) {
+	POSITION status_pos = { 0, MAP_WIDTH / 2 + 1 };
+	gotoxy(status_pos);
+	printf("STATUS: %s", status);
+}
